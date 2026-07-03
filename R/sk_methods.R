17304d8e61c327afaf5ef431dbef0f1241827956
deankoch <deankoch@gmail.com>
2026-07-03 12:28:25 -0700

    remove man page for Summary group generic
    the method remains exported and available, there's just no documentation now
    
    this was causing a CRAN check error due to the Rd file having the same filename (up to capitalization) as another. Since I don't want to deprecate either function, I'm just removing the method's documentation and stuffing it in this dev folder until I can find a workaround.

--- R/sk_methods.R
+++ R/sk_methods.R
@@ -279,51 +279,6 @@ Ops.sk = function(e1, e2)
 }
 
 #'
-#' Summary group generics
-#'
-#' Computes a summary statistic of the grid data values.
-#'
-#' @param ... further arguments to Summary
-#' @param na.rm logical, whether to remove `NA`s before doing the computation
-#'
-#' @return a "sk" object with data values
-#' @export
-#'
-#' @examples
-#'
-#' # make test example and verify common summary stats
-#' g = sk(gval=1+abs(stats::rnorm(4^2)), gdim=4, gres=0.5)
-#' sum(g) == sum(g[])
-#' min(g) == min(g[])
-#' max(g) == max(g[])
-#'
-#' # list input can include scalar, vector (but first argument must be sk class)
-#' sum(g, -g)
-#' sum(g, -g, 0)
-#'
-#' # calculate product in two ways
-#' prod(g) - exp(sum(log(g)))
-#'
-#' # check logical conditions
-#' any(g>0)
-#' all(g>0)
-#'
-#' # count the number of grid points satisfying a condition
-#' sum(g < 2)
-#'
-Summary.sk = function(..., na.rm=FALSE)
-{
-  # extract all grid data values and omit NA's if requested
-  vals = do.call(c, lapply(list(...), function(x) x[]))
-  if(na.rm) vals = vals[ !is.na(vals) ]
-  if( length(vals) == 0 ) stop('all values are NA')
-
-  # dispatch to default method then replace the data values in output sk object
-  .Primitive(.Generic)(vals)
-}
-
-
-#'
 #' Auto-printing
 #'
 #' Prints dimensions and indicates if the grid has values assigned
@@ -374,6 +329,19 @@ print.sk = function(x, ...)
   cat(paste(gdim_msg, complete_msg))
 
 
+}
+
+
+#' @export
+Summary.sk = function(..., na.rm=FALSE)
+{
+  # extract all grid data values and omit NA's if requested
+  vals = do.call(c, lapply(list(...), function(x) x[]))
+  if(na.rm) vals = vals[ !is.na(vals) ]
+  if( length(vals) == 0 ) stop('all values are NA')
+
+  # dispatch to default method then replace the data values in output sk object
+  .Primitive(.Generic)(vals)
 }
 
 #'
